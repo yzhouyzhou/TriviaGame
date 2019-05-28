fedb7661993654653c00f73769fe968f29f41258
Ying Zhou <yzhouyzhou@yahoo.com>
2019-05-27 22:05:26 -0500

    move back to mp4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,42 +3,46 @@ var questionAnswers = [
         question: "1- Who won the world series in 2016?",
         answers: ["Blackhawks", "Cubs", "Bears", "White Soxs"],
         rightAnswer: "Cubs",
-        animate: "./assets/images/giphy-cubs-gif.webp"
+        // animate: "./assets/images/giphy-cubs-gif.webp"
+        animate: "./assets/images/giphy_cubs.mp4"
     },
     {
         question: "2- Name the world's largest ocean.",
         answers: ["Altlantic", "Indian", "Pacific", "Arctic"],
         rightAnswer: "Pacific",
-        animate: "./assets/images/giphy-pacific-gif.webp"
+        // animate: "./assets/images/giphy-pacific-gif.webp"
+        animate: "./assets/images/giphy_pacific.mp4"
     },
     {
         question: "3- Name the world's longest river.",
         answers: ["Amazon", "Yangtze", "Mississippi", "Yenisei"],
         rightAnswer: "Amazon",
-        animate: "./assets/images/giphy-river-gif.webp"
+        // animate: "./assets/images/giphy-river-gif.webp"
+        animate: "./assets/images/giphy_amazon.mp4"
     },
     {
         question: "4- Name the world's largest city.",
         answers: ["Tokyo", "Beijing", "London", "New York City"],
         rightAnswer: "New York City",
-        animate: "./assets/images/giphy-newyork-gif.webp"
+        // animate: "./assets/images/giphy-newyork-gif.webp"
+        animate: "./assets/images/giphy_newyork.mp4"
     },
     {
         question: "5- Who won the World Cup in 2018?",
         answers: ["America", "France", "Italy", "Germany"],
         rightAnswer: "France",
-        animate: "./assets/images/giphy-soccer-gif.webp"
+        // animate: "./assets/images/giphy-soccer-gif.webp"
+        animate: "./assets/images/giphy_soccer.mp4"
     }
-
 ]
-
+// var unCorrectImg = "./assets/images/giphy-wrong-gif.webp";
+var unCorrectImg = "./assets/images/giphy_wrong_answer.mp4";
 var index = 0;
 var correctAnswers = 0;
 var inCorrectAnswers = 0;
 var unAnswers = 0;
 var timer = 30;
 var intervalId;
-var unCorrectImg = "./assets/images/giphy-wrong-gif.webp";
 var isAnswered = false;
 
 $(document).ready(function () {
